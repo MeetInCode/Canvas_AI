c9b1e5c0b83d59d18cadb3326df73b29a4885047
MeetInCode <meets.studentlife@gmail.com>
2025-04-02 23:27:26 +0530

    Add framer-motion dependency and update Home page layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,6 @@
+import { ContainerScroll } from "@/components/globals/Component-scroll";
 import Navbar from "@/components/globals/navbar";
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
@@ -8,12 +10,37 @@ export default function Home() {
 
       <section className="h-screen w-full bg-neutral-950 rounded-md !overflow-visible relative flex flex-col items-center antialiased">
         <div className="absolute inset-0 h-full w-full items-center px-5 py-24 bg-[radial-gradient(125%_125%_at_50%_10%,#000_35%,#223_100%)]"></div>
-      </section>
+     
       <div className="flex flex-col mt-[-100px] md:mt-[-50px]">
       {
-        //continue with component-scroll
+        //continue with component-scroll 
       }
+      <ContainerScroll
+      titleComponent={<div className="flex items-center flex-col">
+        <Button
+          size={'lg'}
+          className="p-8 mb-8 md:mb-0 text-2xl w-full sm:w-fit border-t-2 rounded-full border-[#4D4D4D] bg-[#1F1F1F] hover:bg-white group transition-all flex items-center justify-center gap-4 hover:shadow-xl hover:shadow-neutral-500 duration-500"
+        >
+          <span className="bg-clip-text text-transparent bg-gradient-to-r from-neutral-500 to-neutral-600  md:text-center font-sans group-hover:bg-gradient-to-r group-hover:from-black goup-hover:to-black">
+            Start For Free Today
+          </span>
+        </Button>
+        <h1 className="text-5xl md:text-8xl  bg-clip-text text-transparent bg-gradient-to-b from-white to-neutral-600 font-sans font-bold">
+          Automate Your Work With Fuzzie
+        </h1>
+      </div>}
+      ></ContainerScroll>
       </div>
+      </section>
+
+
+      <section>{
+        //infinite moving cards 33:41
+        }</section>
+
+<section>{
+        //hero section
+        }</section>
     </main>
   );
 }
